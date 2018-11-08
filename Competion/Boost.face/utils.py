52c991c786fb2dd4cf0d7403890b755d199a576d
Denis Zuenko <neron.v2@gmail.com>
2018-11-08 12:48:58 +0300

    Update

--- Competion/Boost.face/utils.py
+++ Competion/Boost.face/utils.py
@@ -1,12 +1,62 @@
 from sklearn.metrics import confusion_matrix
 import numpy as np
+import warnings
 
 def balanced_accuracy_score(y_true, y_pred, sample_weight=None,
                             adjusted=False):
-    
+    """Compute the balanced accuracy
+    The balanced accuracy in binary and multiclass classification problems to
+    deal with imbalanced datasets. It is defined as the average of recall
+    obtained on each class.
+    The best value is 1 and the worst value is 0 when ``adjusted=False``.
+    Read more in the :ref:`User Guide <balanced_accuracy_score>`.
+    Parameters
+    ----------
+    y_true : 1d array-like
+        Ground truth (correct) target values.
+    y_pred : 1d array-like
+        Estimated targets as returned by a classifier.
+    sample_weight : array-like of shape = [n_samples], optional
+        Sample weights.
+    adjusted : bool, default=False
+        When true, the result is adjusted for chance, so that random
+        performance would score 0, and perfect performance scores 1.
+    Returns
+    -------
+    balanced_accuracy : float
+    See also
+    --------
+    recall_score, roc_auc_score
+    Notes
+    -----
+    Some literature promotes alternative definitions of balanced accuracy. Our
+    definition is equivalent to :func:`accuracy_score` with class-balanced
+    sample weights, and shares desirable properties with the binary case.
+    See the :ref:`User Guide <balanced_accuracy_score>`.
+    References
+    ----------
+    .. [1] Brodersen, K.H.; Ong, C.S.; Stephan, K.E.; Buhmann, J.M. (2010).
+           The balanced accuracy and its posterior distribution.
+           Proceedings of the 20th International Conference on Pattern
+           Recognition, 3121-24.
+    .. [2] John. D. Kelleher, Brian Mac Namee, Aoife D'Arcy, (2015).
+           `Fundamentals of Machine Learning for Predictive Data Analytics:
+           Algorithms, Worked Examples, and Case Studies
+           <https://mitpress.mit.edu/books/fundamentals-machine-learning-predictive-data-analytics>`_.
+    Examples
+    --------
+    >>> from sklearn.metrics import balanced_accuracy_score
+    >>> y_true = [0, 1, 0, 0, 1, 0]
+    >>> y_pred = [0, 1, 0, 0, 0, 1]
+    >>> balanced_accuracy_score(y_true, y_pred)
+    0.625
+    """
     C = confusion_matrix(y_true, y_pred, sample_weight=sample_weight)
     with np.errstate(divide='ignore', invalid='ignore'):
         per_class = np.diag(C) / C.sum(axis=1)
+    if np.any(np.isnan(per_class)):
+        warnings.warn('y_pred contains classes not in y_true')
+        per_class = per_class[~np.isnan(per_class)]
     score = np.mean(per_class)
     if adjusted:
         n_classes = len(per_class)
